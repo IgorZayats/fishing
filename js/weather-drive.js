33489a016ea48a62ff05b3ca68303b847c899530
igorzayats <igor.zayatz@gmail.com>
2016-01-30 18:59:07 +0000

    changes

--- js/weather-drive.js
+++ js/weather-drive.js
@@ -1,4 +1,5 @@
 ﻿ $("#fivedays").click(function hide16() {
+   $("#allweather").addClass('allweather')
    $("#weatherTable16 li").remove();
    $("#city").empty();
    $("#blockweather16").css('display', 'none');
@@ -7,6 +8,7 @@
  });
 
  $("#sixteenthdays").click(function hide5() {
+   $("#allweather").addClass('allweather');
    $("#weatherTable5 li").remove();
    $("#city").empty();
    $("#blockweather5").css('display', 'none');
